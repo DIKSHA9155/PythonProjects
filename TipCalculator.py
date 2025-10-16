0e0c4c6608c4e8e0f5d40a3e56c16c195ed7f33e
DIKSHA9155 <dikshabharti934@gmail.com>
2025-10-16 07:01:12 +0530

    Add basic tip calculator functionality

---
+++ TipCalculator.py
@@ -0,0 +1,6 @@
+print("Welcome to the tip calculator!")
+bill=input("What was the toal bill?")
+tip=input("How much tip would you like to give? 10, 12, or 15?") 
+total_bill=((bill*tip)/100)+bill
+split_amount=input("How many people to split the bill?")
+print(f"Each person should pay: {split_amount}")
