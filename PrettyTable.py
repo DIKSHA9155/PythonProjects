1a72512ac2451cf2e48e6511fcd4d3d6beef0473
DIKSHA9155 <dikshabharti934@gmail.com>
2025-10-16 06:47:39 +0530

    Delete PrettyTable.py

--- PrettyTable.py
+++
@@ -1,8 +0,0 @@
-from prettytable import PrettyTable
-
-table = PrettyTable()
-table.add_column("Pokemon Name", ["Squirtle", "Charmander", "Pikachu"])
-table.add_column("Type", ["Water", "Fire", "Electric"])
-table.align = "l"
-print(table)
-
